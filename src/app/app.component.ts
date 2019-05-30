a177eb3459c9ab9dbf015643efec65bceca0fcbd
Suchit <rajrikhy2@gmail.com>
2019-05-30 08:12:33 -0600

    changes:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,7 @@ import {
   NgZone
 } from '@angular/core';
 
-let loadedMonaco = false;
-let loadPromise: Promise<void>;
-declare const require: any;
-declare const monaco: any;
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-root',
@@ -36,54 +33,12 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    this.loadMonaco();
+    this.initMonaco();
   }
 
   changeLanguage() {
     this.language = this.language === 'javascript' ? 'json' : 'javascript';
     this.initMonaco();
-  }
-
-  loadMonaco() {
-    if (loadedMonaco) {
-      // Wait until monaco editor is available
-      loadPromise.then(() => {
-        this.initMonaco();
-      });
-    } else {
-      loadedMonaco = true;
-      loadPromise = new Promise<void>((resolve: any) => {
-        const baseUrl = '/assets';
-        if (typeof (<any>window).monaco === 'object') {
-          resolve();
-          return;
-        }
-        const onGotAmdLoader: any = () => {
-          // Load monaco
-          (<any>window).require.config({
-            paths: { vs: `${baseUrl}/monaco/vs` }
-          });
-          (<any>window).require(['vs/editor/editor.main'], () => {
-            console.log('Monaco is loaded!');
-            this.initMonaco();
-            resolve();
-          });
-        };
-
-        // Load AMD loader if necessary
-        if (!(<any>window).require) {
-          const loaderScript: HTMLScriptElement = document.createElement(
-            'script'
-          );
-          loaderScript.type = 'text/javascript';
-          loaderScript.src = `${baseUrl}/monaco/vs/loader.js`;
-          loaderScript.addEventListener('load', onGotAmdLoader);
-          document.body.appendChild(loaderScript);
-        } else {
-          onGotAmdLoader();
-        }
-      });
-    }
   }
 
   initMonaco() {
